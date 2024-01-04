2f1c6022f797b2541b4182e59e5b05deda7d5936
tranvanson5 <tranvanson231099@gmail.com>
2024-01-05 04:29:51 +0700

    create new project: spring, angular, reactjs.

---
+++ webapp_springboot/src/main/java/com/example/webapp_springboot/WebappSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.webapp_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappSpringbootApplication.class, args);
+	}
+
+}
---
+++ webapp_springboot/src/test/java/com/example/webapp_springboot/WebappSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webapp_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
